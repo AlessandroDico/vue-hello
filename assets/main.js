24a0b2e91588334009404efb03edfbc673fd659b
Ale <alessandrochb@gmail.com>
2020-11-06 14:50:27 +0100

    stampato messaggio in pagina

--- assets/main.js
+++ assets/main.js
@@ -5,10 +5,16 @@
 //CI SONO ALCUNE "VARIABILI" DA INSERIRE ALL'INTERNO DELLA VARIAVILE APP
 //DATA : PER METTERE I DATI IN VUE
 //METHOD: PER METTERE LE FUNZIONI IN VUE
+
+// ---------esercizio----------
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
 var app = new Vue({
     el: '#root',
     data : {
-
+        myClass: 'saluto',
+        message: 'Hello World',
     }
 });
 
